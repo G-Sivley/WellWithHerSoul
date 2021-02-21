ada35f75e4b67601b71af49f000c5466852b8038
Grant Sivley <grant.sivley@gmail.com>
2021-02-20 21:24:23 -0700

    Customize the main screen so that the background was blue.

--- WellWithHerSoul/Views/ContentView.swift
+++ WellWithHerSoul/Views/ContentView.swift
@@ -13,31 +13,36 @@ struct ContentView: View {
     
     @ObservedObject var networkManager = NetworkManger()
     
+    // I want listViewCells background to be: UIColor(red: 0.27, green: 0.37, blue: 0.45, alpha: 1.00)
+    
+    init() {
+        // uses UIColor -- This is the background of the whole thing
+        UITableView.appearance().backgroundColor = UIColor(red: 0.19, green: 0.30, blue: 0.39, alpha: 1.00)
+        }
+    
     var body: some View {
         NavigationView {
             ZStack {
-                Color(red: 0.0, green: 1.0, blue: 0.0)
                 List(networkManager.posts, rowContent: { (post) in
                     NavigationLink(
                         destination: DetailView(url: post.link),
                         label: {
                             HStack {
                                 Text(post.title.rendered)
+                                    .listRowBackground(Color.blue)
                             }
-                            
                         })
-                    
-                    
                 })
                 .navigationBarTitle("Well With Her SOUL")
+                
             }
+            
             .onAppear(perform: {
                 self.networkManager.fetchData()
             })
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
